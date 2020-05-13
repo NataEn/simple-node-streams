909f58c2c800184944f9e804f864716c4c9543ee
natalie <natalie@enkin.co.il>
2020-05-13 15:56:35 +0300

    adding simple streams

---
+++ fileReader.js
@@ -0,0 +1,21 @@
+// in the termonal run this file with node and specify
+//the file you want to read
+// e.g. node fileReader files/myNotes.txt
+
+const fs = require("fs");
+
+const filePath = process.argv[2];
+if (!filePath) {
+  console.log("please provide a file to read from");
+  process.exit(1);
+}
+//if path provided then read the file
+const readStream = fs.createReadStream(filePath);
+readStream.on("data", (chunk) => {
+  console.log("starting to read the file...");
+  console.log(chunk.toString());
+  console.log(`read ${chunk.toString().length} chars in the file`);
+});
+readStream.on("end", () => {
+  console.log("finishe reading the file");
+});
